330f7eed9ee412f83922a2c3e50f27a844a25762
Antoine DEBRAY <antoine.debray@eleves.institutlemonnier.fr>
2023-09-08 08:28:41 +0200

    Ajoutez des fichiers projet.

---
+++ Rationnel/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rationnel
+{
+    public class Class1
+    {
+    }
+}
