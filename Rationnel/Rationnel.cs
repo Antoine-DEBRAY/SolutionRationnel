955ed727dc2ed5931b60f4d97992a2afc747c518
Antoine DEBRAY <antoine.debray@eleves.institutlemonnier.fr>
2023-09-08 09:19:43 +0200

    Création des attributs de la structure Rationnel, ajout du Projet console TestRationnel ainsi que le remplissage de son main

---
+++ Rationnel/Rationnel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rationnel
+{
+    public struct Rationnel
+    {
+        private int denominateur;
+        private int numerateur;
+
+        public int Denominateur { get => denominateur; }
+        public int Numerateur { get => numerateur; }
+        public Rationnel(int n, int d)
+        {
+            if (d == 0)
+            {
+                throw new DivideByZeroException("Le dénominateur ne peut pas être nul");
+            }
+            else
+            {
+                this.numerateur = n;
+                this.denominateur = d;
+            }  
+        }
+        public override string ToString()
+        {
+            StringBuilder chaine = new StringBuilder();
+            chaine.Append(numerateur);
+            chaine.Append(" / ");
+            chaine.Append(denominateur);
+            return chaine.ToString();
+        }
+    }
+}
---
+++ TestRationnel/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestRationnel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Construction de 2 objets de type Rationnel
+                Rationnel.Rationnel premierRationnel = new Rationnel.Rationnel(5, 8);
+                Rationnel.Rationnel secondRationnel = new Rationnel.Rationnel(2, 4);
+
+                // Test de la surcharge de la méthode ToString();
+                Console.WriteLine("*** TEST DE LA METHODE ToString() ***");
+                Console.WriteLine("Voici le premier Rationnel : " + premierRationnel.ToString());
+                Console.WriteLine("Voici le second Rationnel : " + secondRationnel.ToString() + "\n");
+
+                // Test des propriétés sur les deux objets Rationnel
+                Console.WriteLine("*** TEST DES PROPRIETES DES OBJETS ***");
+                Console.WriteLine("Le numérateur du premier Rationnel vaut : " + premierRationnel.Numerateur);
+                Console.WriteLine("Le dénominateur du second Rationnel vaut : " + secondRationnel.Denominateur + "\n");
+
+                // Test de la construction d'un objet Rationnel avec un dénominateur nul
+                Rationnel.Rationnel nulRationnel = new Rationnel.Rationnel(8, 0);
+            }
+            catch(Exception e) { Console.WriteLine(e.Message + "\n"); }
+        }
+    }
+}
