392cf6249cb5579b21e7a35e0b2d846fc978ba86
Antoine DEBRAY <antoine.debray@eleves.institutlemonnier.fr>
2023-09-15 12:07:06 +0200

    Codage des boutons et ajout d'un nouveau bouton

--- WpfTestRationnel/MainWindow.xaml.cs
+++ WpfTestRationnel/MainWindow.xaml.cs
@@ -27,7 +27,54 @@ namespace WpfTestRationnel
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            if ()
+            try
+            {
+                TextBoxRationnel.Clear();
+                if (TextBoxNumerateur.Text == "" || TextBoxDenominateur.Text == "")
+                {
+                    MessageBox.Show("Il faut remplir le Numérateur ET le Dénominateur", "Erreur 305", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                int numerateur = Convert.ToInt32(TextBoxNumerateur.Text);
+                int denominateur = Convert.ToInt32(TextBoxDenominateur.Text);
+                Rationnel.Rationnel rationnel = new Rationnel.Rationnel(numerateur, denominateur);
+                Rationnel.Rationnel reduit = Rationnel.Rationnel.Reduit(rationnel);
+                TextBoxRationnel.Text = reduit.ToString();
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+        }
+
+        private void BoutonValeurDécimale_Click(object sender, RoutedEventArgs e)
+        {
+            try {
+                TextBoxRationnel.Clear();
+                if (TextBoxNumerateur.Text == "" || TextBoxDenominateur.Text == "")
+                {
+                    MessageBox.Show("Il faut remplir le Numérateur ET le Dénominateur", "Erreur 305", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                int numerateur = Convert.ToInt32(TextBoxNumerateur.Text);
+                int denominateur = Convert.ToInt32(TextBoxDenominateur.Text);
+                Rationnel.Rationnel rationnel = new Rationnel.Rationnel(numerateur, denominateur);
+                double mondouble = (double)rationnel;
+                TextBoxRationnel.Text = mondouble.ToString();
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                TextBoxRationnel.Clear();
+                if (TextBoxNumerateur.Text == "" || TextBoxDenominateur.Text == "")
+                {
+                    MessageBox.Show("Il faut remplir le Numérateur ET le Dénominateur", "Erreur 305", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                int numerateur = Convert.ToInt32(TextBoxNumerateur.Text);
+                int denominateur = Convert.ToInt32(TextBoxDenominateur.Text);
+                Rationnel.Rationnel rationnel = new Rationnel.Rationnel(numerateur, denominateur);
+                TextBoxRationnel.Text = rationnel.ToString();
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
         }
     }
 }
